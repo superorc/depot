e3df0f001621ce4cc67da395e5aacb9d3db3b800
superorc <rykanoga@gmail.com>
2015-06-29 13:25:26 +0500

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates_length_of :title, :minimum => 10
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,11 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+			@update = {
+				title: 'Maza Faka',
+				description: 'some descriptoion of fucking book',
+				price: 14.50
+			}
   end
 
   test "should get index" do
@@ -18,9 +23,8 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, price: @product.price, title: @product.title }
+			post :create, product: @update
     end
-
     assert_redirected_to product_path(assigns(:product))
   end
 
@@ -35,7 +39,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    patch :update, id: @product, product: { description: @product.description, price: @product.price, title: @product.title }
+    patch :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
