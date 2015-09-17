ba440b8e41e6b154e448e2cf26b0a386f180a32e
Gregor Zamza <rykanoga@gmail.com>
2015-09-17 13:27:45 +0500

    Cart, sessions

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,22 @@ class Product < ActiveRecord::Base
 	validates :title, uniqueness: true
 	validates :title, length: { minimum: 5 }
 
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	def self.latest
 		Product.order(:updated_at).last
 	end
-	
+
+	private
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'существует товарная позиция')
+			return false
+		end
+	end
+
 end
