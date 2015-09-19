814dc7e70d2cd53aae228d229da29e01b85fd933
superorc <rykanoga@gmail.com>
2015-09-19 18:58:25 +0500

    AJAX cart, with bags

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -59,7 +59,7 @@ class CartsController < ApplicationController
       @cart.destroy if @cart.id ==session[:cart_id]
       session[:cart_id] = nil
       respond_to do |format|
-        format.html { redirect_to store_url, notice: 'Cart was successfully deleted.' }
+        format.html { redirect_to store_url }
         format.json { head :no_content }
       end
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Product.delete_all
-Product.create!(title: 'Ruby book',
-								
-                description: %{Enhanced Tiles customize a Firefox 
-									user’s new tab page, making it unique to that user. 
-                	Firefox will display Tiles based on a user’s browsing history, 
-                  and the experience is easy to customize and organize with a simple, 
-                  drag-and-drop interface.},
 
+Product.create!(title: 'Ruby book',
+                description: %{Enhanced Tiles customize a Firefox
+									user’s new tab page, making it unique to that user.
+                	Firefox will display Tiles based on a user’s browsing history,
+                  and the experience is easy to customize and organize with a simple,
+                  drag-and-drop interface.},
 								price: 49.95)
+
+Product.create!(title: 'Shameless',
+								description: 'Чем красивее история, тем быстрее она становится легендой.
+									Многие мифы сложены настолько стройно и хорошо, что даже не тянет
+									проверять их правдоподобность: хочется лишь поскорее поделиться ими со всеми.
+									Именно потому, что кто-то в свое время отлично сочинял такие захватывающие истории,
+									Питер и оброс множеством легенд и заблуждений. Докопаемся до истины?',
+								price: 120.50)
+
+Product.create!(title: 'Naruto',
+								description: 'манга Масаси Кисимото в жанре сёнэн и её аниме-экранизация.
+									Главным её героем является Наруто Удзумаки, шумный и непоседливый ниндзя-подросток,
+									который мечтает достичь всеобщего признания и стать Хокагэ — главой своего селения
+									и сильнейшим ниндзя. Чтобы добиться уважения окружающих, ему предстоит пройти через
+									тысячи препятствий: экзамены ниндзя, различные миссии и сражения.',
+								price: 23.10)
