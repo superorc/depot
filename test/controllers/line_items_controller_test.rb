f64ed0afee9b867a4e52a7a56c203d84eead27b6
Gregor Zamza <rykanoga@gmail.com>
2015-09-21 12:37:48 +0500

    orders

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -24,13 +24,13 @@ class LineItemsControllerTest < ActionController::TestCase
   end
 
   test "should create line_item via AJAX" do
-    assert_difference(LineItem.count) do
+    assert_difference('LineItem.count') do
       xhr :post, :create, product_id: products(:ruby).id
     end
 
     assert_response :success
-    assert_select_jquery :hrml, '#cart' do
-      assert_select 'tr#current_item id', /Programming ruby 1.9/
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming ruby 1.9/
     end
   end
 
