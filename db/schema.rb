f9d3727d77c07cc52241ade877f738f2adefaa1a
Gregor Zamza <rykanoga@gmail.com>
2015-09-22 17:39:26 +0500

    auth sessions beta

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921053627) do
+ActiveRecord::Schema.define(version: 20150922082621) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20150921053627) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
